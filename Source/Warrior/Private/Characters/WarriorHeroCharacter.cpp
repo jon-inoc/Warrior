5f90d95f0c77b7b85557d968a047a4d427c339bd
jon-inoc <76927315+jon-inoc@users.noreply.github.com>
2025-05-23 15:26:30 -0400

    ADDED: DebugHelper

--- Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
+++ Source/Warrior/Private/Characters/WarriorHeroCharacter.cpp
@@ -2,4 +2,10 @@
 
 
 #include "Characters/WarriorHeroCharacter.h"
+#include "WarriorDebugHelper.h"
 
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	Debug::Print(TEXT("Working"));
+}
