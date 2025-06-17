e38530e05ae2a67fb226b5d3bffba9e964822c20
jon-inoc <76927315+jon-inoc@users.noreply.github.com>
2025-06-17 16:49:58 -0400

    Added attribute sets through Gameplay Effects for hero and enemy base.

--- Source/Warrior/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
+++ Source/Warrior/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
@@ -9,6 +9,8 @@
 class UWarriorGameplayAbility;
 class UWarriorAbilitySystemComponent;
 
+class UGameplayEffect;
+
 /**
  * 
  */
@@ -27,5 +29,8 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
 	TArray<TSubclassOf<UWarriorGameplayAbility>> ReactiveAbilities;
 
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+	TArray< TSubclassOf < UGameplayEffect > > StartUpGameplayEffects;
+	
 	void GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& InAbilitiesToGive,UWarriorAbilitySystemComponent* InASCToGive,int32 ApplyLevel = 1);
 };
